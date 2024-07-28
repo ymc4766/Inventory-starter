e40452e19273678899bb5c16d35847b98439d4f7
yusto <yuusufmahud@gmail.com>
2024-07-28 12:45:35 +0300

    frontend starter setup

---
+++ src/components/Landing.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Landing = () => {
+  const bgImage =
+    "https://images.pexels.com/photos/4481258/pexels-photo-4481258.jpeg?auto=compress&cs=tinysrgb&w=600";
+  return <div>Landing</div>;
+};
+
+export default Landing;
